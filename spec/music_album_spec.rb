c4074d3bc319ba2b6f9b3e9ab5bf21a650fc19e5
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-05 17:31:08 +0200

    Create unit testing for the misic_album class

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,31 +3,31 @@ require_relative '../musics/label'
 
 describe MusicAlbum do
   before :each do
-    @rockstar = Label.new('rockstar', 'inc')
-    @gta = MusicAlbum.new(true, '2018-11-10', '2007-11-10')
-    @rockstar.add_item(@gta)
+    @mysic = Label.new('Gospel', 'Red')
+    @release = MusicAlbum.new(true, '2007-11-10')
+    @mysic.add_item(@release)
   end
 
   it 'shows instance of MusicAlbum class' do
-    expect(@gta).to be_instance_of MusicAlbum
+    expect(@release).to be_instance_of MusicAlbum
   end
 
-  # it 'shows instance of Label class' do
-  #   expect(@rockstar).to be_instance_of Label
-  # end
+  it 'shows instance of Label class' do
+    expect(@mysic).to be_instance_of Label
+  end
 
-  # it 'is multiplayer ?' do
-  #   expect(@gta.multiplayer).to match true
-  # end
+  it 'is on_spotify ?' do
+    expect(@release.on_spotify).to match true
+  end
 
-  # it 'adds gta to rockstar' do
-  #   expect(@rockstar.items.length).to match 1
-  #   expect(@rockstar.items).to include @gta
-  # end
+  it 'adds release to mysic' do
+    expect(@mysic.items.length).to match 1
+    expect(@mysic.items).to include @release
+  end
 
-  # it 'does not add again the same instance' do
-  #   @prev_size = @rockstar.items.length
-  #   @rockstar.add_item(@gta)
-  #   expect(@rockstar.items.length).to match @prev_size
-  # end
+  it 'does not add again the same instance' do
+    @prev_size = @mysic.items.length
+    @mysic.add_item(@release)
+    expect(@mysic.items.length).to match @prev_size
+  end
 end
