14ca41d15b90d2ad9e06472fdf49c117fc6cecb7
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-07 21:32:37 +0200

    Create new instance of the SaveMusicAlbums and pass music_albumsto the save as argument

--- save_and_exit.rb
+++ save_and_exit.rb
@@ -1,6 +1,9 @@
+require_relative './music_albums/save_music_albums'
 require_relative './games/save_games'
+
 class SaveAndExit
-  def store(_books, _music, games)
+  def store(_books, music_albums, games)
+    SaveMusicAlbums.new.save(music_albums)
     SaveGames.new.save(games)
     puts 'thank you for using this app'
   end
