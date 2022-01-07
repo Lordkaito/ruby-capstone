8592fd03c8bfd9c92bfab246a04fcce06a65377a
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-07 21:29:34 +0200

    Add SaveMusicAlbums class to write the music_albums.json

---
+++ music_albums/save_music_albums.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+class SaveMusicAlbums
+  def save(array)
+    savings = []
+    array.each do |music_album|
+      saving_obj = {
+        name: music_album.name,
+        on_spotify: music_album.on_spotify,
+        publish_date: music_album.publish_date,
+        label_info: fetch_label(music_album)
+      }
+      savings << saving_obj
+    end
+    store_tojson(savings)
+  end
+
+  def fetch_label(music_album)
+    if music_album.label
+      music_album.label.title
+      # music_album.label.color
+    else
+     false
+    end
+  end
+
+  def store_tojson(savings)
+    File.write('music_albums/music_albums.json', JSON.generate(savings)) unless savings.empty?
+  end
+end
