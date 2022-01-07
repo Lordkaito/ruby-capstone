854ab2e0071736ec00511eb3ab58a4ea33baf605
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-07 21:23:18 +0200

    Add LoadData method and pass music_albums and labelsas arguments

--- load_data.rb
+++ load_data.rb
@@ -1,6 +1,19 @@
 require 'json'
+require_relative './music_albums/load_music_albums'
 require_relative './games/load_games'
+
+# class LoadData
+#   def load(music_albums, labels, games, sources)
+#     LoadMusicAlbums.new.load_music_albums(music_albums, labels)
+#     LoadGames.new.load_games(games, sources)
+#   end
+# end
+
 class LoadData
+  def load_music(music_albums, labels)
+    LoadMusicAlbums.new.load_music_albums(music_albums, labels)
+  end
+
   def load(games, sources)
     LoadGames.new.load_games(games, sources)
   end
